da55d28a78e59ff94b0528b80f4ce4e4587aa8dd
Fatemeh Ostad <f.ostad@gmail.com>
2016-03-24 01:34:42 +0100

    moved main stuff to GameOfLife class

--- src/com/gameoflife/GameOfLife.java
+++ src/com/gameoflife/GameOfLife.java
@@ -2,7 +2,98 @@ package com.gameoflife;
 
 import java.awt.Color;
 
+import com.gameoflife.controller.GameController;
+import com.gameoflife.controller.GameControllerImpl;
+import com.gameoflife.model.Cell;
+import com.gameoflife.model.GameBoard;
+import com.gameoflife.view.GameView;
+
 public class GameOfLife {
+
+	private int boardHight;
+	private int boardWidth;
+	private int timeInterval;
+	private Color boardColor;
+	private String boardName;
+	
+	private int cellSize;
+	private Color cellColor;
+	
+	public GameOfLife(int boardHight, int boardWidth, int timeInterval, Color boardColor, int cellSize,
+			Color cellColor) {
+		super();
+		this.boardHight = boardHight;
+		this.boardWidth = boardWidth;
+		this.timeInterval = timeInterval;
+		this.boardColor = boardColor;
+		this.cellSize = cellSize;
+		this.cellColor = cellColor;
+	}
+
+	public void startGame() {
+		GameBoard gameBoard = new GameBoard(boardHight, boardWidth, timeInterval,boardColor);
+		Cell cell = new Cell(cellSize, cellColor, boardWidth, boardHight);
+		GameController gameController = new GameControllerImpl();
+		gameController.initializeCells(cell);
+		GameView gameView = new GameView(gameBoard, cell, gameController);
+		gameView.createPanel();
+	}
+
+	public int getBoardHight() {
+		return boardHight;
+	}
+
+	public void setBoardHight(int boardHight) {
+		this.boardHight = boardHight;
+	}
+
+	public int getBoardWidth() {
+		return boardWidth;
+	}
+
+	public void setBoardWidth(int boardWidth) {
+		this.boardWidth = boardWidth;
+	}
+
+	public int getTimeInterval() {
+		return timeInterval;
+	}
+
+	public void setTimeInterval(int timeInterval) {
+		this.timeInterval = timeInterval;
+	}
+
+	public Color getBoardColor() {
+		return boardColor;
+	}
+
+	public void setBoardColor(Color boardColor) {
+		this.boardColor = boardColor;
+	}
+
+	public String getBoardName() {
+		return boardName;
+	}
+
+	public void setBoardName(String boardName) {
+		this.boardName = boardName;
+	}
+
+	public int getCellSize() {
+		return cellSize;
+	}
+
+	public void setCellSize(int cellSize) {
+		this.cellSize = cellSize;
+	}
+
+	public Color getCellColor() {
+		return cellColor;
+	}
+
+	public void setCellColor(Color cellColor) {
+		this.cellColor = cellColor;
+	}
 	
 	
 }
--- src/com/gameoflife/Main.java
+++ src/com/gameoflife/Main.java
@@ -10,8 +10,8 @@ import com.gameoflife.view.GameView;
 
 public class Main {
 
-	private static final int HIGHT = 15;
-	private static final int WIDTH = 15; 
+	private static final int BOARD_HIGHT = 15;
+	private static final int BOARD_WIDTH = 15; 
 	private static final int TIME_INTERVAL = 1000;
 	private static final String BOARD_NAME = "Game of Life";
 	private static final Color BOARD_COLOR = Color.lightGray;
@@ -20,13 +20,9 @@ public class Main {
 	private static final Color CELL_COLOR = Color.BLUE;
 	
 	public static void main(String[] args) {
-		GameBoard gameBoard = new GameBoard(HIGHT, WIDTH, TIME_INTERVAL,BOARD_COLOR);
-		gameBoard.setBoardName(BOARD_NAME);
-		Cell cell = new Cell(CELL_SIZE, CELL_COLOR, WIDTH, HIGHT);
-		GameController gameController = new GameControllerImpl();
-		gameController.initializeCells(cell);
-		GameView gameView = new GameView(gameBoard, cell, gameController);
-		gameView.createPanel();
+		GameOfLife gameOfLife = new GameOfLife(BOARD_HIGHT, BOARD_WIDTH, TIME_INTERVAL, BOARD_COLOR, CELL_SIZE, CELL_COLOR);
+		gameOfLife.setBoardName(BOARD_NAME);
+		gameOfLife.startGame();
 	}
 
 }
